2be18d9f2ebb0507aa5d5aabf08f7deddc0ae063
Reid Atcheson <reid.atcheson@gmail.com>
2016-10-23 10:33:10 -0500

    added coqide script

--- src/ContinuousFunction.v
+++ src/ContinuousFunction.v
@@ -1,4 +1,5 @@
 Require Import Coq.QArith.QArith_base.
+Require Import Psatz.
 Require Import Sequence.
 
 
@@ -19,7 +20,7 @@ Record continuous_function := {
 
 Definition fex1 (x:Q) := x.
 Definition wex1 (x:Q) := x.
-Lemma wex1_works : forall q : Q, q>=0 -> 
+Lemma w_spec1_ex1 : forall q : Q, q>=0 -> 
     forall x y, (abs ((fex1 x)+ -(fex1 y))) 
     <= wex1 (abs (x + -y)).
 Proof.
@@ -30,11 +31,17 @@ destruct (Qlt_le_dec (x + -y) 0).
 * apply Qle_refl.
 Qed.
 
+Lemma w_spec2_ex2 : wex1 0 == 0.
+Proof.
+unfold wex1.
+lra.
+Qed.
+
 Definition ex1 : continuous_function.
 Proof.
 (*eauto using Build_continuous_function, wex1_works.*)
 apply Build_continuous_function with (f:=fex1) (w:=wex1).
-apply wex1_works.
+apply w_spec1_ex1 .
 Qed.
 
 
